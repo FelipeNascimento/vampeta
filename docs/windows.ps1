a76eebc298ae2cc8b27af411ffd4259a268a6cb5
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2018-07-25 02:37:12 -0300

    ..

---
+++ docs/windows.ps1
@@ -0,0 +1,5 @@
+folder=$HOME/pvt
+file=$folder/love.jpg
+mkdir $folder
+curl https://f.i.uol.com.br/fotografia/2013/10/31/331934-970x600-1.jpeg > $file
+osascript -e 'tell application "System Events" to tell every desktop to set picture to "'$file'"'
--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mac: "curl " + document.location.href + "mac.sh > install.sh && bash install.sh && rm -f install.sh"
+    }
+    this.copy = this.copy.bind(this)
+  }
+  copy(e) {
+    e.target.select()
+    document.execCommand("copy");
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Wallpaper do vampeta</h1>
-          <img src={process.env.PUBLIC_URL + "/love.jpg"} style={{maxHeight:100}}/>
+          <img src={process.env.PUBLIC_URL + "/love.jpg"} style={{ maxHeight: 100 }} />
         </header>
         <h1>Mac book OSX</h1>
-        <text>{"curl " + document.location.href + "mac.sh > install.sh && bash install.sh && rm -f install.sh"}</text>
+        <input className="link" type="text" value={this.state.mac} id="mac" onClick={this.copy} />
       </div>
     );
   }
 }
+
 export default App;
