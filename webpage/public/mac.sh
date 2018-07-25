6e322874eae5b4526d0c5a59383223abac11b434
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2018-07-25 14:22:30 -0300

    ...

--- webpage/public/mac.sh
+++ webpage/public/mac.sh
@@ -1,5 +1,4 @@
 folder=/Library/Desktop Pictures
 file=$folder/love.jpg
-mkdir $folder
 curl https://f.i.uol.com.br/fotografia/2013/10/31/331934-970x600-1.jpeg > $file
 osascript -e 'tell application "System Events" to tell every desktop to set picture to "'$file'"'
