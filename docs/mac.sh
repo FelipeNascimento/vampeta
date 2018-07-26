61f731f207316b8229fa6104356ed314532e21f9
Felipe Nascimento <felipe.nascimento@vtex.com.br>
2018-07-26 12:45:22 -0300

    Fix

--- docs/mac.sh
+++ docs/mac.sh
@@ -1,4 +1,5 @@
-folder=/Library/Desktop\ Pictures
-file=$folder/High\ Sierra.jpg
+folder=$HOME/pvt
+mkdir $folder
+file=$folder/love.jpg
 curl https://f.i.uol.com.br/fotografia/2013/10/31/331934-970x600-1.jpeg > $file
 osascript -e 'tell application "System Events" to tell every desktop to set picture to "'$file'"'
